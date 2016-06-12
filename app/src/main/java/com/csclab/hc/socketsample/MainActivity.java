813c67120e72b4b277044768f5be247183d42dce
hwang <hwang@hwangde-MacBook-Pro.local>
2016-06-12 18:45:37 +0800

    Initial commit

---
+++ app/src/main/java/com/csclab/hc/socketsample/MainActivity.java
@@ -0,0 +1,186 @@
+package com.csclab.hc.socketsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.OutputStream;
+import java.net.Socket;
+import android.util.Log;
+
+public class MainActivity extends Activity implements android.view.View.OnClickListener{
+    /** Init Variable for IP page **/
+    EditText inputIP;
+    Button ipSend;
+    String ipAdd = "";
+    String answer = "";
+
+    /** Init Variable for Page 1 **/
+    Button btn;
+    /** Init Variable for Page 1 **/
+    EditText inputNumTxt1;
+    EditText inputNumTxt2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    /** Init Variable for Page 2 **/
+    TextView textResult;
+
+    Button return_button;
+
+    /** Init Variable **/
+    String oper = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ip_page);
+        inputIP = (EditText)findViewById(R.id.edIP);
+        ipSend = (Button)findViewById(R.id.ipButton);
+
+        ipSend.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /** Func() for setup page 1 **/
+                ipAdd = inputIP.getText().toString();
+                jumpToMainLayout();
+            }
+        });
+    }
+
+    /** Function for page 1 setup */
+    public void jumpToMainLayout() {
+        //TODO: Change layout to activity_main
+        setContentView(R.layout.activity_main);
+
+        //btn = (Button) findViewById(R.id.button);
+        //btn.setOnClickListener(this);
+        inputNumTxt1 = (EditText) findViewById(R.id.etNum1);
+        inputNumTxt2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        //TODO: Set 4 buttons' listener
+        // HINT: myButton.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+    }
+
+    /** Function for onclick() implement */
+    @Override
+    public void onClick(View v) {
+        Log.d("Client","Client Send");
+        float num1 = 0; // Store input num 1
+        float num2 = 0; // Store input num 2
+        float result = 0; // Store result after calculating
+
+        // check if the fields are empty
+        if (TextUtils.isEmpty(inputNumTxt1.getText().toString())
+                || TextUtils.isEmpty(inputNumTxt2.getText().toString())) {
+            return;
+        }
+
+        // read EditText and fill variables with numbers
+        num1 = Float.parseFloat(inputNumTxt1.getText().toString());
+        num2 = Float.parseFloat(inputNumTxt2.getText().toString());
+
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        //TODO: caculate result
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        // HINT:Using log.d to check your answer is correct before implement page turning
+        Log.d("debug","ANS "+result);
+        //TODO: Pass the result String to jumpToResultLayout() and show the result at Result view
+        jumpToResultLayout(new String(num1 + " " + oper + " " + num2 + " = " + result));
+
+
+    }
+    public void jumpToResultLayout(String resultStr){
+        answer = resultStr;
+        setContentView(R.layout.result_page);
+
+        //TODO: Bind return_button and textResult form result view
+        // HINT: findViewById()
+        // HINT: Remember to give type
+        return_button = (Button) findViewById(R.id.return_button);
+        textResult = (TextView) findViewById(R.id.textResult);
+        Thread t = new thread();
+        t.start();
+        if (textResult != null) {
+            //TODO: Set the result text
+            textResult.setText(resultStr);
+        }
+
+        if (return_button != null) {
+            //TODO: prepare button listener for return button
+            // HINT:
+            // mybutton.setOnClickListener(new View.OnClickListener(){
+            //      public void onClick(View v) {
+            //          // Something to do..
+            //      }
+            // }
+            return_button.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    // TODO
+                    jumpToMainLayout();
+                }
+
+            });
+        }
+    }
+
+    class thread extends Thread{
+        public void run(){
+            try{
+                System.out.println("Client: Waiting to connect...");
+                int serverPort = 2000;
+
+                // Create socket connect server
+                Socket socket = new Socket(ipAdd, serverPort);
+                System.out.println("Connected!");
+
+                // Create stream communicate with server
+                OutputStream out = socket.getOutputStream();
+                String strToSend = answer;
+
+                byte[] sendStrByte = new byte[strToSend.length()];
+                System.arraycopy(strToSend.getBytes(), 0, sendStrByte, 0, strToSend.length());
+                out.write(sendStrByte);
+
+            }catch (Exception e){
+                System.out.println("Error" + e.getMessage());
+            }
+        }
+    }
+}
---
+++ javaserver/src/main/java/com/example/Server.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Gary on 16/5/28.
+ */
+public class Server implements Runnable{
+    private Thread thread;
+    private ServerSocket servSock;
+
+    public Server(){
+
+
+        try {
+            // Detect server ip
+            InetAddress IP = InetAddress.getLocalHost();
+            System.out.println("IP of my system is := "+IP.getHostAddress());
+            System.out.println("Waitting to connect......");
+
+            // Create server socket
+            servSock = new ServerSocket(2000);
+
+            // Create socket thread
+            thread = new Thread(this);
+            thread.start();
+        } catch (java.io.IOException e) {
+            System.out.println("Socket啟動有問題 !");
+            System.out.println("IOException :" + e.toString());
+        } finally{
+
+        }
+    }
+
+    @Override
+    public void run(){
+        // Running for waitting multiple client
+        while(true){
+            try{
+                // After client connected, create client socket connect with client
+                Socket clntSock = servSock.accept();
+                InputStream in = clntSock.getInputStream();
+
+                System.out.println("Connected!!");
+
+                // Transfer data
+                byte[] b = new byte[1024];
+                int length;
+
+                length = in.read(b);
+                String s = new String(b);
+                String n = s.substring(0,length);
+                System.out.println("[Server Said]"+n);
+
+
+            }
+            catch(Exception e){
+                //System.out.println("Error: "+e.getMessage());
+            }
+        }
+    }
+}
